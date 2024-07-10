cfdcc6352369a7f2340586f8c033ab6dd7cf842a
Alhagie Bai Cham <cham207388@gmail.com>
2024-07-10 16:35:14 -0500

    output

--- keypair/outputs.tf
+++ keypair/outputs.tf
@@ -1,7 +1,7 @@
-output "key-pair-id" {
+output "key_pair_id" {
   value = aws_key_pair.generated_key.key_pair_id
 }
 
-output "key-pair-name" {
+output "key_pair_name" {
   value = aws_key_pair.generated_key.key_name
 }
