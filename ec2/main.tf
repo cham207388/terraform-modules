4d2524cbd5b103f8e987b1655dde8520ec80b6ea
Alhagie Bai Cham <cham207388@gmail.com>
2024-07-10 16:07:12 -0500

    refactor

--- ec2/main.tf
+++ ec2/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "ec2" {
   ami                         = var.ami
   instance_type               = var.instance_type
   vpc_security_group_ids      = var.vpc_security_group_ids
-  user_data                   = file("./ec2-user-data.sh")
+  user_data                   = file("ec2-user-data.sh")
   subnet_id                   = var.subnet_id
   associate_public_ip_address = var.associate_public_ip_address
   tags                        = var.tags
