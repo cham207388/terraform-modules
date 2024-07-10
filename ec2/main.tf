cae4fac425a3b1735917b9d052deba6179b4d48c
Alhagie Bai Cham <cham207388@gmail.com>
2024-07-10 15:38:09 -0500

    refactor

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "ec2" {
-  ami                         = var.ami_id
+  ami                         = var.ami
   instance_type               = var.instance_type
   vpc_security_group_ids      = var.vpc_security_group_ids
   user_data                   = var.user_data
   subnet_id                   = var.subnet_id
-  associate_public_ip_address = var.associate_public_ip
+  associate_public_ip_address = var.associate_public_ip_address
   tags                        = var.tags
   key_name                    = var.key_name
   iam_instance_profile        = aws_iam_instance_profile.role_profile.id
--- ec2/variables.tf
+++ ec2/variables.tf
@@ -1,4 +1,4 @@
-variable "ami_id" {
+variable "ami" {
   default = "ami-06c68f701d8090592"
 }
 variable "instance_type" {
@@ -8,7 +8,7 @@ variable "vpc_security_group_ids" {
   type = list(any)
 }
 variable "user_data" {}
-variable "associate_public_ip" {
+variable "associate_public_ip_address" {
   type = bool
 }
 variable "subnet_id" {}
