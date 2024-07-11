7c27f773e427342608764aa1549146d2387d4d4d
Alhagie Bai Cham <cham207388@gmail.com>
2024-07-11 13:11:37 -0500

    provider module

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,20 +1,32 @@
+# resource "aws_instance" "my_instance" {
+#   ami                         = var.ami
+#   instance_type               = var.instance_type
+#   user_data                   = <<-EOF
+#               #!/bin/bash
+#               sudo yum update -y
+#               sudo yum install -y httpd
+#               sudo systemctl start httpd
+#               sudo systemctl enable httpd
+#               echo "<h1>Hello World from $(hostname -f)</h1> > /var/www/html/index.html
+#               EOF
+#   subnet_id                   = var.subnet_id
+#   vpc_security_group_ids      = var.vpc_security_group_ids
+#   associate_public_ip_address = var.associate_public_ip_address
+#   tags                        = var.tags
+#   key_name                    = var.key_name
+#   iam_instance_profile        = aws_iam_instance_profile.role_profile.id
+# }
+
 resource "aws_instance" "my_instance" {
   ami                         = var.ami
   instance_type               = var.instance_type
-  vpc_security_group_ids      = var.vpc_security_group_ids
-  user_data                   = <<-EOF
-              #!/bin/bash
-              sudo yum update -y
-              sudo yum install -y httpd
-              sudo systemctl start httpd
-              sudo systemctl enable httpd
-              echo "<h1>Hello World from $(hostname -f)</h1> > /var/www/html/index.html
-              EOF
   subnet_id                   = var.subnet_id
   associate_public_ip_address = var.associate_public_ip_address
-  tags                        = var.tags
+  vpc_security_group_ids      = var.vpc_security_group_ids
   key_name                    = var.key_name
-  iam_instance_profile        = aws_iam_instance_profile.role_profile.id
+  user_data                   = file("./user-data.sh")
+
+  tags = var.tags
 }
 
 resource "aws_iam_instance_profile" "role_profile" {
