20f00a389abb6b94d1fc6204b115b3a516fc3dbf
Alhagie Bai Cham <cham207388@gmail.com>
2024-07-11 20:16:59 -0500

    key_name

--- keypair/outputs.tf
+++ keypair/outputs.tf
@@ -1,7 +1,7 @@
-output "keypair_id" {
+output "key_id" {
   value = aws_key_pair.generated_key.key_pair_id
 }
 
-output "keypair_name" {
+output "key_name" {
   value = aws_key_pair.generated_key.key_name
 }
---
+++ security-group/variables.tf
@@ -0,0 +1,5 @@
+variable "vpc_id" {}
+variable "ingress" {
+  type = number
+}
+variable "name" {}
