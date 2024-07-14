bf16c69a3fcd16112ecfff3183655b27b27e0835
Alhagie Bai Cham <cham207388@gmail.com>
2024-07-13 19:59:54 -0500

    cidr_blocs in sg module

--- security-group/main.tf
+++ security-group/main.tf
@@ -5,13 +5,13 @@ resource "aws_security_group" "this" {
     from_port   = var.ingress
     to_port     = var.ingress
     protocol    = "tcp"
-    cidr_blocks = "0.0.0.0/0"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = "0.0.0.0/0"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
     Name = var.name
