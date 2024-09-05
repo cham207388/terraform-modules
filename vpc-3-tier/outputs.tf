886c0bac113e93e04515b25a7a6ef5eb199ba124
Alhagie Bai Cham <cham207388@gmail.com>
2024-09-05 10:42:38 -0500

    dynamic az cidr

--- vpc-3-tier/outputs.tf
+++ vpc-3-tier/outputs.tf
@@ -1,3 +1,13 @@
 output "azs" {
   value = slice(data.aws_availability_zones.available.names, 0, 2)
 }
+
+output "public_subnet_cidrs" {
+  value = local.public_subnet_cidrs
+}
+output "private_subnet_cidrs" {
+  value = local.private_subnet_cidrs
+}
+output "database_subnet_cidrs" {
+  value = local.database_subnet_cidrs
+}
